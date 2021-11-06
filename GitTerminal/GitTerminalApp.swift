af6b5cc3016b36165d1106df180f6069d1134b07
Juan372008 <90113494+Juan372008@users.noreply.github.com>
2021-11-06 15:19:09 +0100

    initial commit

---
+++ GitTerminal/GitTerminalApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTerminalApp.swift
+//  GitTerminal
+//
+//  Created by Juan Gallo on 6/11/21.
+//
+
+import SwiftUI
+
+@main
+struct GitTerminalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
